417e49d4302452d574ef8fbf7ccbf632ce1e765a
Khush Patel <ikhushpatel@gmail.com>
2018-12-26 01:22:53 +0530

    V3
    V3

--- js/drop-custom.js
+++ js/drop-custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var IntroDrop, ExperienceDrop, ProjectsDrop, CertificationsDrop, HonorsDrop, NewsDrop, HomeDrop;
+	var IntroDrop, ExperienceDrop, ProjectsDrop, CertificationsDrop, HonorsDrop, NewsDrop, HomeDrop, ConnectDrop;
 	IntroDrop = new Drop({
 	    target: document.querySelector('.Intro-ico'),
 	    content: 'Intro',
@@ -42,4 +42,50 @@ $(document).ready(function(){
 		position: 'top center',
 		openOn: 'hover'
 	});
+	ConnectDrop = new Drop({
+		target: document.querySelector('.dummyActionButtonConnect'),
+		content: '',
+		position: 'top center',
+		openOn: 'hover'
+	});
+});
+
+var $item = $('.carousel-item');
+var $wHeight = $(window).height();
+
+$item.height($wHeight);
+$item.addClass('full-screen');
+
+var $numberofSlides = $('.carousel-item').length;
+var $currentSlide = Math.floor((Math.random() * $numberofSlides));
+
+$('.carousel-indicators li').each(function(){
+  var $slideValue = $(this).attr('data-slide-to');
+  if($currentSlide == $slideValue) {
+    $(this).addClass('active');
+    $item.eq($slideValue).addClass('active');
+  } else {
+    $(this).removeClass('active');
+    $item.eq($slideValue).removeClass('active');
+  }
+});
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 1000,
+  pause: "false"
 });
